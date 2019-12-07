a1d90cd3667b2b59fc5a3ac12017d8b28cefa916
ksmaybe <ksmaybe@gmail.com>
2019-12-07 14:02:02 -0500

    sss

--- gogo.py
+++ gogo.py
@@ -1,15 +1,16 @@
 import mysql.connector
 db_connection = mysql.connector.connect(
     host= "localhost",
-    user= "root",
-    passwd= "root"
+    user= "user",
+    passwd= ""
 )
 # creating database_cursor to perform SQL operation
 db_cursor = db_connection.cursor()
 # executing cursor with execute method and pass SQL query
-db_cursor.execute("CREATE DATABASE my_first_db")
 # get list of all databases
 db_cursor.execute("SHOW DATABASES")
 #print all databases
 for db in db_cursor:
     print(db)
+
+
