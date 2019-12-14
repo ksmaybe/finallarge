498e99ab1637f6a443640415b0fae3a5ee829256
ksmaybe <ksmaybe@gmail.com>
2019-12-14 10:03:46 -0500

    mapreduce part 1

--- gogo.py
+++ gogo.py
@@ -1,10 +1,12 @@
 import mysql.connector
+import csv
+import os
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="hello",
-  passwd='',
-  database='db'
+    host="localhost",
+    user="hello",
+    passwd='',
+    database='db'
 )
 
 mycursor = mydb.cursor()
@@ -21,6 +23,29 @@ mycursor = mydb.cursor()
 #   mycursor.execute(sql,val)
 
 # mydb.commit()
-mycursor.execute("select * from MOCK_DATA")
-for row in mycursor:
-  print(row[0],row[1])
+go =1001
+with os.scandir('data/') as data:
+    for file in data:
+        fe='data/'+file.name
+        lst=[]
+        with open(fe) as f:
+            reader=csv.reader(f,delimiter=',')
+            for row in reader:
+                lst.append(row)
+
+        print(file.name)
+        sql="insert into MOCK_DATA (id,topic) values (%s,%s)"
+        for row in lst[1:]:
+            val=(str(go),row[1])
+            mycursor.execute(sql,val)
+            go+=1
+mydb.commit()
+
+#
+# mycursor.execute("select id from MOCK_DATA order by id desc limit 1")
+# for row in mycursor:
+#     print(row[0])
+
+# mycursor.execute("select id,topic from MOCK_DATA")
+# for row in mycursor:
+#     print(row[0],row[1])
--- mapreduce2.py
+++ mapreduce2.py
@@ -1,5 +1,33 @@
-import sys
+import mysql.connector
 import csv
+import os
+import functools
+import time
 
-iteration=0
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="hello",
+    passwd='',
+    database='db'
+)
+start_time=time.time()
+mycursor = mydb.cursor()
+
+mycursor.execute("select id,topic from MOCK_DATA")
+data=[]
+freq={}
+for row in mycursor:
+    data.append(row[1])
+    if row[1] in freq:
+        freq[row[1]]+=1
+    else:
+        freq[row[1]]=1
+f=[]
+for key in freq:
+    f.append((key,freq[key]))
+f.sort(key=lambda x:x[1],reverse=True)
+print(f[:20])
+print(time.time()-start_time," nano seconds")
+
+
 
